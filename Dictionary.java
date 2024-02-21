f1d597f8cad36a77d0812ec776232fb73943d8a1
Alexander DeAngelis <ajd61@students.uwf.edu>
2024-02-21 13:19:58 -0600

    Logic done

---
+++ Dictionary.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+// Documentation up next
+public class Dictionary {
+
+	private List<String> WordList;
+	private int WordCount;
+	private String FileName;
+	
+	public Dictionary(String FileName) {
+		this.FileName = FileName;
+		this.WordCount = 0;
+		WordList = new ArrayList<String>();
+	}
+	
+	public boolean populateWordList() {
+		try {
+			BufferedReader fileReader = new BufferedReader(new FileReader(FileName));
+			
+			String str = fileReader.readLine();
+			while ((str != null) && (str.length() != 0)) {
+				
+				if (str.length() >= 3)
+					WordList.add(str.toLowerCase());
+				
+				str = fileReader.readLine();
+			}
+			
+			fileReader.close();
+			
+			this.WordCount = WordList.size();
+			
+			return true;
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public String getWord() {
+		Random rand1 = new Random();
+		int seed2 = rand1.nextInt();
+		Random rand2 = new Random(seed2);
+		int seed3 = rand2.nextInt();
+		Random rand3 = new Random(seed3);
+		int index = rand3.nextInt(WordCount);
+		
+		return WordList.get(index);
+	}
+	
+}
