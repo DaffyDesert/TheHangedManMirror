a9f1cc680e9abe5d43fad20f5c912665b0ecc348
bkemily <107584119+bkemily@users.noreply.github.com>
2024-04-17 20:45:11 -0400

    Update MainMenuPanel.java

--- src/MainMenuPanel.java
+++ src/MainMenuPanel.java
@@ -138,21 +138,20 @@ public class MainMenuPanel extends JPanel {
         JTextArea textArea = new JTextArea("Game Objective:\r\n" + //
                         "Guess the word correctly before the hangman is fully drawn.\r\n\n" + //
                         "How To Play:\r\n" + //
-                        "Guess the word correctly before the hangman is fully drawn.\r\n" + //
                         "Pick your choice of difficulty to begin playing.\r\n" + //
                         "A word is randomly selected, and your goal is to guess it letter by letter.\r\n" + //
                         "Guess the entire word or letters one at a time. If the guess is correct, it will be revealed. If not, a part of the hangman is drawn.\r\n" + //
                         "Keep guessing until you either guess the word correctly or the hangman is fully drawn.\r\n\n" + //
                         "Features:\r\n" + //
-                        "Difficulty Levels: Choose from a range of difficulty levels, from novice to expert, to suit your skill level.\r\n" + //
+                        " -Difficulty Levels: Choose from a range of difficulty levels, from novice to expert.\r\n" + //
                         "   -The Fool: less than 6 letters per word\r\n" + //
                         "   -Strength: 6 - 8 letters per word with at least one\r\n" + //
                         "    letter repeated\r\n" + //
                         "   -Death: 7+ letters per word with no more than 1\r\n" + //
                         "    letter repeated more than once\r\n" + //
                         "   -The World: Every word in the dictionary is possible.\r\n" + //
-                        "Themes: Immerse yourself in various themes that bring a fresh aesthetic to the classic gameplay.\r\n" + //
-                        "Sound Effects: Let the sound effects heighten the tension as you race against the hangman's creation.\r\n" + //
+                        " -Themes: Immerse yourself in various themes that bring a fresh aesthetic to the classic gameplay.\r\n" + //
+                        " -Sound Effects: Let the sound effects heighten the tension as you race to victory.\r\n" + //
                         "");
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
