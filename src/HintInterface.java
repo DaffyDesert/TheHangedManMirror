8f221b4f8896f9ef36c64c9e4463b0e60829dc6c
AlexDeAngelisUWF <aj.espersen419@gmail.com>
2024-04-22 13:36:20 -0500

    Created and implemented HintInterface and HintGenerator class

---
+++ src/HintInterface.java
@@ -0,0 +1,8 @@
+
+public interface HintInterface {
+	public void calculateDeduction();
+	public void changeWord(String word);
+	public void generateHint();
+	public char getHint();
+	public int getPointDeduction();
+}
